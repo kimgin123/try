1768b584b23321aeb94771d1feda527abecaab44
Gina Kim <iamsoggo@gmail.com>
2023-02-21 00:34:00 -0500

    I'm done

---
+++ src/cardgamew23tueam/Try.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cardgamew23tueam;
+
+/**
+ *
+ * @author kimjeongrok
+ */
+
+public class Try extends Card{
+    
+    public Try(Card.Value value, Card.Suit suit) {
+        super(value, suit);
+    }
+    
+}
+
